650dd6f4032092a57eb5fdcf6eb96c2fc5f6a62f
davidkim234 <league@WTS-IM-6.attlocal.net>
2018-07-12 14:58:19 -0700

    Day 4

--- src/section4/DragonFight.java
+++ src/section4/DragonFight.java
@@ -9,57 +9,68 @@ public class DragonFight {
 	public static void main(String[] args) {
 		
 		ImageIcon dragon = new ImageIcon("src/section4/dragonPicture.jpg");		
-		
+		Random ranGen=new Random();
 		JOptionPane.showMessageDialog(null, "Defeat the dragon to take its treasure!", "Dragon Fighter", 0, dragon);
 		// 2. Create a variable called "playerHealth" to store your health (set it equal to 100)
-	
+		int playerHealth = 100;
 		// 3. Create a variable called "dragonHealth" to store the dragon's health (set it equal to 100)
-		
+		int dragonHealth = 100;
 		// 4. Create a variable to hold the damage the player's attack does each round
-		
+		int PD = 7;
 		// 5. Create a variable to hold the damage the dragon's attack does each round
+		int DD = 14;
 		
-		
+		JOptionPane.showMessageDialog(null, "Punches can deal damage from 5 - 15 \n Kicks deal anywhere from 0 - 25");
 		// 6.  Delete the slashes at the beginning of the next line.  
-		//while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
-		
+		while(playerHealth>0 && dragonHealth>0) {    //this line of code keeps the battle going until someone's health reaches 0 
+			JOptionPane.showMessageDialog(null, "New round \n Your Health: "+playerHealth+"\n Dragon Health:"+dragonHealth);
 		// 7. Add a closing mustache at the very bottom of this program (since we just added an opening mustache on the previous step).
 		
 		// 8. Ask the player in a pop-up if they want to attack the dragon with a yell or a kick
-		
+		String AC = JOptionPane.showInputDialog("Would you like to Punch or Kick?");
 		// 9. If they typed in "yell":
-		
+		if(AC.equalsIgnoreCase("punch")) {
 			//-- Find a random number between 0 and 10 and store it in dragonDamage
-			
+			PD = ranGen.nextInt(10)+5;
 			//-- Subtract that number from the dragon's health variable 
-			
+		dragonHealth-=PD;
+		JOptionPane.showMessageDialog(null,"your attack did: "+PD+"\n Dragon Health:"+dragonHealth);
+		}
 		// 10. If they typed in "kick":
-		
+		if(AC.equalsIgnoreCase("kick")) {
 			//-- Find a random number between 0 and 25 and store it in dragonDamage
-			
+			PD = ranGen.nextInt(26);
 			//-- Subtract that number from the dragon's health variable
-			
+			dragonHealth-=PD;
+			JOptionPane.showMessageDialog(null, "your attack did: "+PD+"\n Dragon Health:"+dragonHealth);
+		}
 			
 		
 		// 11.  Find a random number between 0 and 35 and store it in playerDamage
-		
+		DD = ranGen.nextInt(36);
 		// 12. Subtract this number from the player's health
-		
+		playerHealth-=DD;
+		JOptionPane.showMessageDialog(null, "The Dragon's attack did: "+DD+"\n Your Health:"+playerHealth);
+		}
 		
 		
 		// 13. If the user's health is less than or equal to 0
-		
+		if(playerHealth <= 0) {
+			JOptionPane.showMessageDialog(null, "you lose!");
+		}
 			//-- Tell the user that they lost
 			
 		
 		// 14. Else if the dragon's health is less than or equal to 0
-		
-			//-- Tell the user that the dragon is dead and they took a ton of gold!
-			
-	   //  15.  Else
-			
-			//-- Pop up a message that tells the their current health and the dragon's currently health (Bonus: Also display the amount of health that was lost for each player this round)
-			
+		if(dragonHealth<= 0) {
+			JOptionPane.showMessageDialog(null, "you win the GOLD!");
 		}
-	}
+			//-- Tell the user that the dragon is dead and they took a ton of gold!
+		else {
+	   //  15.  Else
+			JOptionPane.showMessageDialog(null, "Final Score \nYour Health:" + playerHealth + "\n Dragon Health:" + dragonHealth);}
+			//-- Pop up a message that tells the their current health and the dragon's currently health (Bonus: Also display the amount of health that was lost for each player this round)
+		}
+		}
+	
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,22 +1,34 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score = 0;
 		// 2.  Ask the user a question 
-		
+		String answer = JOptionPane.showInputDialog("What is the center peak or dip in a parabola called?");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if(answer.equalsIgnoreCase ("the vertex")) {
+		score++;
 		// 4.  if the user's answer is correct
-		
+		}
 		// -- add one to their score 
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
+		String answer2 = JOptionPane.showInputDialog("What is the cause of a black hole's unlimited gravity");
 		
+		if(answer2.equalsIgnoreCase ("the singularity")) {
+		score++;
+		}
+		
+		String answer3 = JOptionPane.showInputDialog("what tense is this in? : Juan jugaras videojuegos este semana. (answer in english)");
 		// 6.  After all the questions have been asked, print the user's score 
-		
+		if(answer3.equalsIgnoreCase ("the future tense")) {
+			score++;
+			}
+	JOptionPane.showMessageDialog(null, "your score is..."+ score +"/3");
 	}
 }
